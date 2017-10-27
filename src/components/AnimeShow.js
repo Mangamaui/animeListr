2b38eabd799cb5c7cc3017052bbdcfd8093655af
Claudia Reynders <exotic.maui@gmail.com>
2017-10-27 16:53:08 +0200

    Created animeShow component

---
+++ src/components/AnimeShow.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+class AnimeShow extends React.Component {
+    
+    render() {
+        const IMG = "/images/" + this.props.cover;
+
+        return (
+            <li className="animeShow">
+                <img src={IMG} />
+                <p className="animeShow__title">{this.props.title}</p>
+                <div className="episode-counter">
+                    <span className="episode-counter__progress">0</span>|
+                    <span className="episode-counter__total">{this.props.episodes}</span>
+                </div>
+            </li>
+        )
+    }
+}
+
+export default AnimeShow;
