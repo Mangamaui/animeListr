096d2b529134a8aaba5cbf9691ba08916148d2eb
Claudia Reynders <exotic.maui@gmail.com>
2017-10-27 16:00:30 +0200

    Basic setup with create-react-app

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <header>
+          <h1>AnimeListr</h1>
+        </header>
+        <div class="content">
+            
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
