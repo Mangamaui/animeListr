56d2c0f85956b780a8c28935fcb8ea75f2197d7c
Claudia Reynders <exotic.maui@gmail.com>
2017-10-28 15:11:44 +0200

    Created store for app
    Added app store

---
+++ src/lib/store.js
@@ -0,0 +1,5 @@
+import { createStore, combineReducers } from 'redux';
+import reducers from '../reducers';
+
+export default createStore(combineReducers(reducers),
+window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
