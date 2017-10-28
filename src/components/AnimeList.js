245406a4dcf3202c2f27afda890fc790d4cb7811
Claudia Reynders <exotic.maui@gmail.com>
2017-10-28 15:12:55 +0200

    Connected AnimeList

--- src/components/AnimeList.js
+++ src/components/AnimeList.js
@@ -1,26 +1,24 @@
 import React from 'react';
 import animeCatalog from '../lib/animeCatalog';
 
+import { connect } from 'react-redux';
 import AnimeShow from './AnimeShow';
 
 
 class AnimeList extends React.Component {
 
-    
+  render() {
 
-    render() {
+    const LIST = animeCatalog.map(function(show){
+      return <AnimeShow key={show.id} title={show.title} cover={show.cover} episodes={show.episodes} />;
+    });
 
-        const LIST = animeCatalog.map(function(show){
-            return <AnimeShow key={show.id} title={show.title} cover={show.cover} episodes={show.episodes} />;
-        });
-
-        return (
-            <ul className="animeList">
-                {LIST}
-            </ul>
-        )
-
-    }
+    return (
+      <ul className="animeList">
+      {LIST}
+      </ul>
+    )
+  }
 }
 
-export default AnimeList;
+export default connect()(AnimeList);
