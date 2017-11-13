b80f3f581a4d4b2eff2419adfd3bafe7df9f926e
Claudia Reynders <exotic.maui@gmail.com>
2017-11-13 15:05:14 +0100

    Added new user reducer to root reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,11 @@
 import shows from './shows';
 import userCollection from './userCollection';
+import app from './app';
 
 const reducers = {
     shows: shows,
-    userCollection: userCollection
+    userCollection: userCollection,
+    app: app
 };
 
 export default reducers;
