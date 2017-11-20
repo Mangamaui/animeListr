ee69a2fb7c0e7e92462378fd773fba4327116643
Claudia Reynders <exotic.maui@gmail.com>
2017-11-20 23:51:12 +0100

    Refactored user actions

--- src/actions/app.js
+++ src/actions/app.js
@@ -1,24 +1,38 @@
-import {fetchPost, authenticatedFetch} from '../lib/general';
+import { fetchPost, fetchRequest } from '../lib/general';
+
+const URL = 'http://localhost:4000/users';
 
 export function signup(email, password, username) {
   let data = JSON.stringify({ 'email': email, 'password': password,'user_name': username });
   let token = null;
 
   return(dispatch) => {
-    fetchPost('http://localhost:4000/users', data)
+
+    fetchPost(URL, data)
     .then( response => {
 
-      if(response.jwt) {
+      if (response.jwt) {
         token = response.jwt;
-        authenticatedFetch('http://localhost:4000/users/info',token)
+        let customURL = `${URL}/info`;
+
+        fetchRequest(customURL, 'GET', token)
+        .then((response) => {
+          if (!response.status) {
+            throw Error(response.statusText);
+          }
+
+          dispatch(requestIsLoading(false));
+          return response;
+
+        }).then((response) => response.json())
         .then((userName) => {
           const user = {...userName,token: token};
           dispatch({
             type: 'SIGNUP',
             user: user
           });
-
-        });
+        })
+        .catch(() => dispatch(requestHasErrored(true)));
 
       } else {
         //show error
@@ -36,15 +50,29 @@ export function login(email, password) {
     fetchPost('http://localhost:4000/users/token', data)
     .then((response) => {
 
-      if(response.jwt) {
+      if (response.jwt) {
         token = response.jwt;
-        authenticatedFetch('http://localhost:4000/users/info',token).then((userName) => {
+        let customURL = `${URL}/info`;
+
+        fetchRequest(customURL, 'GET', token)
+        .then((response) => {
+          if (!response.status) {
+            throw Error(response.statusText);
+          }
+
+          dispatch(requestIsLoading(false));
+          return response;
+
+        }).then((response) => response.json())
+        .then((userName) => {
           const user = {...userName,token: token};
           dispatch({
             type: 'LOGIN',
             user: user
           });
-        });
+        })
+        .catch(() => dispatch(requestHasErrored(true)));
+
       } else {
         //dispatch error
       }
