a58f3adb6b7776aceb676bedf3b36127db9db2b6
Claudia Reynders <exotic.maui@gmail.com>
2017-11-20 23:50:00 +0100

    Added userCollection actions

--- src/actions/userCollection.js
+++ src/actions/userCollection.js
@@ -1,34 +1,141 @@
-let nextID = 0;
+import { requestHasErrored, requestIsLoading } from './app';
+import { fetchRequest } from '../lib/general';
 
+const URL = 'http://localhost:4000/user_shows';
 
-export function addShow(showID){
-    return {
-        type: 'ADD_SHOW',
-        id: nextID++,
-        showID: showID
-    }
-}
+export function loadUserCollection(token) {
+  return (dispatch) => {
+    dispatch(requestIsLoading(true));
 
-export function removeShow(showID) {
-    return {
-        type: 'REMOVE_SHOW',
-        showID: showID
-    }
-}
+    fetchRequest(URL,'GET', token)
+    .then((response) => {
+      if (!response.status) {
+        throw Error(response.statusText);
+      }
 
-export function updateShowStatus(showID, status) {
-  return {
-    type: 'UPDATE_SHOW_STATUS',
-    showID: showID,
-    status: status
+      dispatch(requestIsLoading(false));
+      return response;
+
+    }).then((response) => response.json())
+    .then((userCollection) => dispatch({
+      type: 'LOAD_USERCOLLECTION',
+      userCollection: userCollection
+    }))
+    .catch(() => dispatch(requestHasErrored(true)));
   }
 }
 
-export function updateShowProgress(showID, progress, episodes) {
-  return {
-    type: 'UPDATE_SHOW_PROGRESS',
-    showID: showID,
-    progress: progress,
-    episodes: episodes
+export function addShow(show_id, token) {
+  let data = JSON.stringify({'show_id': show_id});
+
+  return (dispatch) => {
+    dispatch(requestIsLoading(true));
+
+    fetchRequest(URL, 'POST',token, data)
+    .then((response) => {
+      if (!response.status) {
+        throw Error(response.statusText);
+      }
+
+      dispatch(requestIsLoading(false));
+      return response;
+
+    }).then((response) => response.json())
+    .then((userShow) => dispatch({
+      type: 'ADD_SHOW',
+      userShow: userShow
+    }))
+    .catch(() => dispatch(requestHasErrored(true)));
   }
+}
+
+export function removeShow(id, token) {
+  let customURL = URL + `/${id}`;
+
+  return (dispatch) => {
+    dispatch(requestIsLoading(true));
+
+    dispatch({
+      type: 'REMOVE_SHOW',
+      id: id
+    });
+
+    fetchRequest(customURL, 'DELETE',token)
+    .then((response) => {
+      if (!response.status) {
+        throw Error(response.statusText);
+      }
+
+      dispatch(requestIsLoading(false));
+      return response;
+
+    }).catch(() => dispatch(requestHasErrored(true)));
+
+  }
+
+}
+
+export function updateShowStatus(id, token, status) {
+  let customURL = URL + `/${id}`;
+
+  let data = JSON.stringify({'status': status });
+
+  return (dispatch) => {
+    dispatch(requestIsLoading(true));
+
+    fetchRequest(customURL, 'PUT',token, data)
+    .then((response) => {
+      if (!response.status) {
+        throw Error(response.statusText);
+      }
+
+      dispatch(requestIsLoading(false));
+      return response;
+
+    }).then((response) => response.json())
+    .then((userShow) => dispatch({
+      type: 'UPDATE_SHOW',
+      userShow: userShow
+    }))
+    .catch(() => dispatch(requestHasErrored(true)));
+  }
+}
+
+export function updateShowProgress(id, token, progress, episodes) {
+  progress = parseInt(progress, 10);
+
+  let customURL = URL + `/${id}`;
+  const STATUS = setShowStatus(progress, episodes);
+
+  let data = JSON.stringify({'status': STATUS, 'progress': progress });
+
+  return (dispatch) => {
+    dispatch(requestIsLoading(true));
+
+    fetchRequest(customURL, 'PUT',token, data).then((response) => {
+      if (!response.status) {
+        throw Error(response.statusText);
+      }
+
+      dispatch(requestIsLoading(false));
+      return response;
+
+    }).then((response) => response.json())
+    .then((userShow) => dispatch({
+      type: 'UPDATE_SHOW',
+      userShow: userShow
+    }))
+    .catch(() => dispatch(requestHasErrored(true)));
+  }
+}
+
+function setShowStatus(progress, episodes) {
+  let status = 0;
+
+  if (progress > 0 && progress < episodes) {
+    status = 1;
+  } else if (progress === episodes) {
+    status = 2;
+  }
+  return status
 }
