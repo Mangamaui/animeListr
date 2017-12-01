71346e32499c52b4a24a9d9cdfbc95d45e2cec49
Claudia Reynders <exotic.maui@gmail.com>
2017-12-01 11:33:36 +0100

    Created a animeCollection component
    (visual component)

---
+++ src/components/AnimeCollection.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import AnimeList from '../containers/AnimeList';
+
+class AnimeCollection extends React.Component {
+  render() {
+    return <AnimeList />;
+  }
+}
+
+export default AnimeCollection;
