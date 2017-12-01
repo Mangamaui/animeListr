f5a973b4ac6e4a900e1f7bbac5154c403c7bfe42
Claudia Reynders <exotic.maui@gmail.com>
2017-12-01 12:01:44 +0100

    Added routes to all views

--- src/index.js
+++ src/index.js
@@ -3,13 +3,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import registerServiceWorker from './registerServiceWorker';
 import { ConnectedRouter } from 'react-router-redux';
-import { Route } from 'react-router';
+import { Route, Switch } from 'react-router';
 import { Provider } from 'react-redux';
 //lib stuff
 import store, { history } from './lib/store';
 import { saveState } from './lib/localStorage';
 //components
 import App from './App';
+import Login from './components/Login';
+import Signup from './components/Signup';
+import AnimeCollection from './components/AnimeCollection';
+import UserCollection from './components/UserCollection';
+import PrivateRoute from './components/PrivateRoute';
+
 //styling
 import './styling/style.css';
 
@@ -17,16 +23,21 @@ store.subscribe(() => {
   saveState({
     app: store.getState().app
   });
-  console.log(store.getState());
 });
+
 
 ReactDOM.render(
   (<Provider store={store}>
     { /* ConnectedRouter will use the store from Provider automatically */ }
     <ConnectedRouter history={history}>
-      <div>
-        <Route exact path="/" component={App}/>
-      </div>
+      <App>
+        <Switch>
+          <Route exact path='/' component={AnimeCollection} />
+          <Route path='/login' component={Login} />
+          <Route path='/signup' component={Signup} />
+          <PrivateRoute path='/userCollection' component={UserCollection} />
+        </Switch>
+      </App>
     </ConnectedRouter>
   </Provider>), document.getElementById('root'));
 registerServiceWorker();
