2b874bb6dae28cbcc67d859f2bd7493cc52c5606
Claudia Reynders <exotic.maui@gmail.com>
2017-12-01 12:01:01 +0100

    Refactored App for use of routes
    - moved authentication check to general
    - Refactored jsx template
    - Deleted old form switcher

--- src/App.js
+++ src/App.js
@@ -1,64 +1,47 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { isTokenExpired } from './lib/general';
 
-import AnimeList from './components/AnimeList';
-import UserForm from './components/UserForm';
-
+import { logout} from './actions/app';
 import { loadAnimeCatalog } from './actions/shows';
 import { loadUserCollection } from './actions/userCollection';
 
+import Header from './components/Header';
 
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     props.dispatch(loadAnimeCatalog());
-
-    this.state = {
-      signup: false
-    }
-
-    this.switchForms = this.switchForms.bind(this);
   }
 
   componentDidMount() {
-    this.getUserCollection();
+    const TOKEN = this.props.authenticated;
+
+    if (isTokenExpired(TOKEN)) {
+      this.props.dispatch(logout());
+
+    } else {
+      this.getUserCollection();
+    }
   }
 
   render() {
-    const AUTHENTICATED = this.props.authenticated != null;
-
-    let userCollection =  null;
-    if (this.props.shows.length > 0 &&  AUTHENTICATED) {
-      userCollection = <AnimeList class="userCollection" />
-    } else {
-      userCollection = "";
-    }
 
     return (
       <div className="App">
-        <header>
-          <div className="user-form-wrap">
-          <button className="form-switch" onClick={this.switchForms}>Create account</button>
-            <UserForm signup={this.state.signup}/>
-          </div>
-          <h1>AnimeListr</h1>
+        <Header authenticated={this.props.authenticated} userName={this.props.userName}/>
+        <main className="content">
 
-        </header>
-        <div className="content">
-            <h2>Anime catalog:</h2>
-            <AnimeList />
-            <hr/>
-            <h2>User collection:</h2>
-            {userCollection}
-        </div>
+        {this.props.isLoading &&
+          <p>THIS IS LOADING...</p>
+        }
+
+          {this.props.children}
+
+        </main>
       </div>
     );
-  }
-
-  switchForms() {
-    const SIGNUP = !this.state.signup ? true : false;
-    this.setState({signup: SIGNUP});
   }
 
   getUserCollection() {
@@ -66,12 +49,17 @@ class App extends React.Component {
      this.props.dispatch(loadUserCollection(this.props.authenticated));
    }
   }
+
 }
+
 
 export default connect(function(state){
   return {
+    location: state.router.location,
     list: state.shows.collection,
     shows: state.userCollection.shows,
-    authenticated: state.app.authenticated
+    authenticated: state.app.authenticated,
+    userName: state.app.user,
+    isLoading: state.app.isLoading
   };
 })(App);
