0436f9c223c87332033cf5864ad5acbcfc4abd91
Claudia Reynders <exotic.maui@gmail.com>
2017-12-07 21:51:57 +0100

    Added sub nav functionality and  updated structure

--- src/components/UserIndicator.js
+++ src/components/UserIndicator.js
@@ -8,18 +8,33 @@ class UserIndicator extends React.Component {
     super(props);
 
     this.handleClick = this.handleClick.bind(this);
+    this.toggleSubNav = this.toggleSubNav.bind(this);
+
+    this.state = {
+      isActive: false
+    }
   }
 
   render() {
+    let classes = "user-indicator__menu sub-nav";
+    if (this.state.isActive) {
+      classes += " active";
+    }
+
     return (
       <li className="user-indicator">
-        <span className="user-indicator__username">{this.props.userName} <i className="icon icon-sort-down"></i></span>
-          <ul className="user-indicator__menu sub-nav">
-            <li><NavLink to="/userCollection">personal collection</NavLink></li>
+        <span className="user-indicator__username" onClick={this.toggleSubNav}>{this.props.userName} <i className="icon icon-sort-down"></i></span>
+          <ul className={classes}>
+            <li><NavLink exact to="/userCollection">personal collection</NavLink></li>
             <li><a onClick={this.handleClick}>logout</a></li>
           </ul>
       </li>
     );
+  }
+
+  toggleSubNav(event) {
+    const STATE = this.state.isActive ? false : true;
+    this.setState({isActive: STATE});
   }
 
   handleClick(event) {
@@ -29,4 +44,9 @@ class UserIndicator extends React.Component {
     this.props.dispatch(reduxAction);
   }
 }
-export default connect()(UserIndicator);
+
+export default connect(function(state){
+  return {
+    location: state.router.location,
+  };
+})(UserIndicator);
