2dec6170106950a671689065c47c04d08c39bf92
Claudia Reynders <exotic.maui@gmail.com>
2017-12-12 13:05:11 +0100

    Removed unused wrapper

--- src/containers/UserForm.js
+++ src/containers/UserForm.js
@@ -24,19 +24,16 @@ class UserForm extends React.Component {
             </label>);
 
     return (
-      <div className="form-wrap">
-        <form className={FORMCLASS} onSubmit={this.handleSubmit}>
-          <h2>{FORMTEXT}</h2>
-          { (SIGNUP)?userNameField:""}
-          <label>Email:<br/>
-            <input type="email" name="email" className="login-form__input form__input" placeholder="mugen@samuraichamploo.com" required={true} onChange={this.handleInputChange} />
-          </label>
-          <label>Password:<br/>
-            <input type="password" name="password" className="login-form__input form__input" required={true} onChange={this.handleInputChange}  />
-          </label>
-          <button className="user-form__btn form-btn">{FORMTEXT}</button>
-        </form>
-      </div>
+      <form className={FORMCLASS} onSubmit={this.handleSubmit}>
+        { (SIGNUP)?userNameField:""}
+        <label>Email:<br/>
+          <input type="email" name="email" className="login-form__input form__input" placeholder="mugen@samuraichamploo.com" required={true} onChange={this.handleInputChange} />
+        </label>
+        <label>Password:<br/>
+          <input type="password" name="password" className="login-form__input form__input" required={true} onChange={this.handleInputChange}  />
+        </label>
+        <button className="user-form__btn form-btn">{FORMTEXT}</button>
+      </form>
     );
   }
 
