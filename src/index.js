37a12f4731dc74b85cb4a75a0ed6a9f95bfda885
Claudia Reynders <exotic.maui@gmail.com>
2018-02-03 11:50:52 +0100

    Removed registerServiceWorker

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import registerServiceWorker from './registerServiceWorker';
+
 import { ConnectedRouter } from 'react-router-redux';
 import { Route, Switch } from 'react-router';
 import { Provider } from 'react-redux';
@@ -40,4 +40,3 @@ ReactDOM.render(
       </App>
     </ConnectedRouter>
   </Provider>), document.getElementById('root'));
-registerServiceWorker();
