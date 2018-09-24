3f2d006b602a2fff5da595ca1550b11b0402ceba
Claudia Reynders <exotic.maui@gmail.com>
2018-09-24 20:58:21 +0200

    Refactored AnimeShow
    - decoupled dispatch from component
    - moved render conditions to a setup function

--- src/containers/AnimeShow.js
+++ src/containers/AnimeShow.js
@@ -1,33 +1,48 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import {
-  addShow
-} from '../actions/userCollection';
+import { bindActionCreators } from 'redux';
+
+import { addShow } from '../actions/userCollection';
 
 
 class AnimeShow extends React.Component {
-  constructor(props) {
-    super(props);
+    constructor(props) {
+        super(props);
+        this.handleBTN = this.handleBTN.bind(this);
+    }
 
-    //bind event handlers
-    this.handleAddEvent = this.handleAddEvent.bind(this);
-  }
+    handleBTN(e) {
+        e.preventDefault();
+        this.props.addShow(this.props.id);
+    }
+
+ setup() {
+     const BTNICON = (this.props.inCollection) ? "tick" : "plus";
+     const CLASSES = `icon icon-${BTNICON}`;
+     const DISABLED = (this.props.inCollection);
+     const ADDBTN = (<button className="action-btn add-btn "
+        onClick={this.handleBTN} 
+        disabled={DISABLED}
+        name="addShow"><i className={CLASSES}></i>
+     </button>);
+
+     const AUTHENTICATED = this.props.authentication != null;
+
+     return {
+         img: "/images/" + this.props.cover,
+         printBtn: AUTHENTICATED ? ADDBTN : ""
+
+     }
+ }
 
   render() {
-    const IMG = "/images/" + this.props.cover;
-    const BTNICON = (this.props.inCollection) ? "tick" : "plus";
-    const CLASSES = `icon icon-${BTNICON}`;
-    const DISABLED = (this.props.inCollection);
-    const ADDBTN = (<button className="action-btn add-btn " onClick={this.handleAddEvent} disabled={DISABLED} name="addShow"><i className={CLASSES}></i></button>);
-
-    const AUTHENTICATED = this.props.authentication != null;
-    const PRINTBTN = AUTHENTICATED ? ADDBTN : "";
+      const settings = this.setup();
 
     return(
       <li className="show-card animeShow">
         <div className="show-card__image-wrap">
-          <img className="show-card__image" src={IMG} alt={this.props.title}/>
-          {PRINTBTN}
+          <img className="show-card__image" src={settings.img} alt={this.props.title}/>
+          {settings.printBtn}
         </div>
         <span className="show-card__info">
           <p className="show-card_title">{this.props.title}</p>
@@ -37,12 +52,12 @@ class AnimeShow extends React.Component {
     )
   }
 
-  handleAddEvent (event) {
-    event.preventDefault();
-    let reduxAction = addShow(this.props.id);
-    this.props.dispatch(reduxAction);
-  }
-
 }
 
-export default connect()(AnimeShow);
+const mapDispatchToProps = (dispatch) => {
+    return {
+        addShow: bindActionCreators(addShow, dispatch)
+    }
+}
+
+export default connect(null, mapDispatchToProps)(AnimeShow);
