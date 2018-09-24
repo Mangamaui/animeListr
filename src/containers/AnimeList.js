5954778f13f720099951e8a2cc02733f8c080463
Claudia Reynders <exotic.maui@gmail.com>
2018-09-24 21:03:01 +0200

    Refactored AnimeList
    - decoupled state from component
    - replaced component props with spread attributes

--- src/containers/AnimeList.js
+++ src/containers/AnimeList.js
@@ -10,24 +10,26 @@ class AnimeList extends React.Component {
 
   render() {
     const USERCLASS = (this.props.class) ? this.props.class : null;
-    const AUTH = this.props.user.authenticated;
+    const AUTH      = this.props.user.authenticated;
 
     const LIST = this.props.list.map((show) => {
       let animeShow = null;
-      const ITEM = this.findShow(show.id);
+      const ITEM    = this.findShow(show.id);
 
       //if the component has the userClass it's a userCollection
       if ((USERCLASS != null) && this.props.shows) {
 
         if (ITEM) {
-
-          animeShow = <UserShow key={"UC"+ITEM.id} id={ITEM.id} title={show.title} cover={show.cover}
-          episodes={show.episodes} userCollectionItem={true} progress={ITEM.progress} status={ITEM.status}/>
+          animeShow = <UserShow key={"UC"+ITEM.id} {...show}
+                                userCollectionItem={true}
+                                {...ITEM} />
         };
         // an AnimeList component without userClass represents the animeCatalog
       } else if (USERCLASS == null) {
         const inCollection = ITEM ? true : false;
-        animeShow = <AnimeShow key={show.id} id={show.id} title={show.title} cover={show.cover} episodes={show.episodes} inCollection={inCollection} authentication={AUTH}/>;
+        animeShow = <AnimeShow key={show.id} {...show}
+                        inCollection={inCollection}
+                        authentication={AUTH} />;
       }
 
       return animeShow;
@@ -49,10 +51,12 @@ class AnimeList extends React.Component {
 
 }
 
-export default connect(function(state) {
-  return {
-    list: state.shows.collection,
-    shows: state.userCollection.shows,
-    user: state.app
-  };
-})(AnimeList);
+const mapStateToProps = (state) => {
+    return {
+        list: state.shows.collection,
+        shows: state.userCollection.shows,
+        user: state.app
+    }
+}
+
+export default connect(mapStateToProps, null)(AnimeList);
